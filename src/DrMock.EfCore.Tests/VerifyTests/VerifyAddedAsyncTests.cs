c709314b140afeba804111a8ee7389f1782cf467
Jeph Bayfield <jeph_b@hotmail.com>
2026-03-22 20:12:21 +0000

    Verify added all passing with multiples as well

--- src/DrMock.EfCore.Tests/VerifyTests/VerifyAddedAsyncTests.cs
+++ src/DrMock.EfCore.Tests/VerifyTests/VerifyAddedAsyncTests.cs
@@ -7,6 +7,74 @@ namespace DrMock.EfCore.Tests.VerifyTests
     public class VerifyAddedAsyncTests
     {
         [Fact]
+        public async Task GivenInterface_WhenAddedAsyncDirect_ShouldVerifyAddedCorrectly()
+        {
+            // Arrange
+            var mock = new MockDbContext<ITestDbContext>()
+                                .WithEntity<Person>();
+
+            var sut = mock.Object;
+
+            var firstName = RandomFirstName();
+            var lastName = RandomLastName();
+
+            Person newPerson = new Person() { FirstName = firstName, LastName = lastName };
+
+            // Act
+            await sut.AddAsync(newPerson);
+
+            // Assert
+            mock.VerifyAddedAsync<Person>(x => x.FirstName == firstName
+                                       && x.LastName == lastName);
+
+            mock.VerifyAddedAsync<Person>(x => x.FirstName == firstName
+                                       && x.LastName == lastName, Times.Once());
+
+            mock.VerifyAddedOnceAsync<Person>(x => x.FirstName == firstName
+                                       && x.LastName == lastName);
+
+            Should.Throw<Exception>(() => mock.VerifyNeverAddedAsync<Person>(x => x.FirstName == firstName
+                                                                          && x.LastName == lastName));
+
+            mock.VerifyNeverAddedAsync<Person>(x => x.FirstName == string.Empty
+                                            && x.LastName == string.Empty);
+        }
+
+        [Fact]
+        public async Task GivenClass_WhenAddedAsyncDirect_ShouldVerifyAddedCorrectly()
+        {
+            // Arrange
+            var mock = new MockDbContext<TestDbContext>()
+                                .WithEntity<Person>();
+
+            var sut = mock.Object;
+
+            var firstName = RandomFirstName();
+            var lastName = RandomLastName();
+
+            Person newPerson = new Person() { FirstName = firstName, LastName = lastName };
+
+            // Act
+            await sut.AddAsync(newPerson);
+
+            // Assert
+            mock.VerifyAddedAsync<Person>(x => x.FirstName == firstName
+                                       && x.LastName == lastName);
+
+            mock.VerifyAddedAsync<Person>(x => x.FirstName == firstName
+                                       && x.LastName == lastName, Times.Once());
+
+            mock.VerifyAddedOnceAsync<Person>(x => x.FirstName == firstName
+                                       && x.LastName == lastName);
+
+            Should.Throw<Exception>(() => mock.VerifyNeverAddedAsync<Person>(x => x.FirstName == firstName
+                                                                          && x.LastName == lastName));
+
+            mock.VerifyNeverAddedAsync<Person>(x => x.FirstName == string.Empty
+                                            && x.LastName == string.Empty);
+        }
+
+        [Fact]
         public async Task GivenInterface_WhenAddedAsyncOnDbSet_ShouldVerifyAddedCorrectly()
         {
             // Arrange
@@ -72,6 +140,126 @@ namespace DrMock.EfCore.Tests.VerifyTests
 
             mock.VerifyNeverAddedAsync<Person>(x => x.FirstName == string.Empty
                                                  && x.LastName == string.Empty);
+        }
+
+        [Fact]
+        public async Task GivenInterface_WhenAddedAsyncMultipleTimesDirect_ShouldVerifyAddedCorrectly()
+        {
+            // Arrange
+            var mock = new MockDbContext<ITestDbContext>()
+                                .WithEntity<Person>();
+
+            var sut = mock.Object;
+
+            var firstName = RandomFirstName();
+            var lastName = RandomLastName();
+
+            Person newPerson = new Person() { FirstName = firstName, LastName = lastName };
+
+            var numberOfEvents = RandomByteBetween(2, 5);
+
+            // Act
+            for (int i = 0; i < numberOfEvents; i++)
+            {
+                await sut.AddAsync(newPerson);
+            }
+
+            // Assert
+            mock.VerifyAddedAsync<Person>(x => x.FirstName == firstName
+                                            && x.LastName == lastName, Times.Exactly(numberOfEvents));
+
+            Should.Throw<Exception>(() => mock.VerifyAddedOnceAsync<Person>(x => x.FirstName == firstName
+                                                                              && x.LastName == lastName));
+        }
+
+        [Fact]
+        public async Task GivenClass_WhenAddedAsyncMultipleTimesDirect_ShouldVerifyAddedCorrectly()
+        {
+            // Arrange
+            var mock = new MockDbContext<TestDbContext>()
+                                .WithEntity<Person>();
+
+            var sut = mock.Object;
+
+            var firstName = RandomFirstName();
+            var lastName = RandomLastName();
+
+            Person newPerson = new Person() { FirstName = firstName, LastName = lastName };
+
+            var numberOfEvents = RandomByteBetween(2, 5);
+
+            // Act
+            for (int i = 0; i < numberOfEvents; i++)
+            {
+                await sut.AddAsync(newPerson);
+            }
+
+            // Assert
+            mock.VerifyAddedAsync<Person>(x => x.FirstName == firstName
+                                            && x.LastName == lastName, Times.Exactly(numberOfEvents));
+
+            Should.Throw<Exception>(() => mock.VerifyAddedOnceAsync<Person>(x => x.FirstName == firstName
+                                                                              && x.LastName == lastName));
+        }
+
+        [Fact]
+        public async Task GivenInterface_WhenAddedAsyncMultipleTimesOnDbSet_ShouldVerifyAddedCorrectly()
+        {
+            // Arrange
+            var mock = new MockDbContext<ITestDbContext>()
+                                .WithEntity<Person>();
+
+            var sut = mock.Object;
+
+            var firstName = RandomFirstName();
+            var lastName = RandomLastName();
+
+            Person newPerson = new Person() { FirstName = firstName, LastName = lastName };
+
+            var numberOfEvents = RandomByteBetween(2, 5);
+
+            // Act
+            for (int i = 0; i < numberOfEvents; i++)
+            {
+                await sut.People.AddAsync(newPerson);
+            }
+
+            // Assert
+            mock.VerifyAddedAsync<Person>(x => x.FirstName == firstName
+                                            && x.LastName == lastName, Times.Exactly(numberOfEvents));
+
+            Should.Throw<Exception>(() => mock.VerifyAddedOnceAsync<Person>(x => x.FirstName == firstName
+                                                                              && x.LastName == lastName));
+        }
+
+        [Fact]
+        public async Task GivenClass_WhenAddedAsyncMultipleTimesOnDbSet_ShouldVerifyAddedCorrectly()
+        {
+            // Arrange
+            var mock = new MockDbContext<TestDbContext>()
+                                .WithEntity<Person>();
+
+            var sut = mock.Object;
+
+            var firstName = RandomFirstName();
+            var lastName = RandomLastName();
+
+            Person newPerson = new Person() { FirstName = firstName, LastName = lastName };
+
+            var numberOfEvents = RandomByteBetween(2, 5);
+
+            // Act
+            for (int i = 0; i < numberOfEvents; i++)
+            {
+                await sut.People.AddAsync(newPerson);
+            }
+
+            // Assert
+            mock.VerifyAddedAsync<Person>(x => x.FirstName == firstName
+                                            && x.LastName == lastName, Times.Exactly(numberOfEvents));
+
+            Should.Throw<Exception>(() => mock.VerifyAddedOnceAsync<Person>(x => x.FirstName == firstName
+                                                                              && x.LastName == lastName));
         }
     }
 }
--- src/DrMock.EfCore.Tests/VerifyTests/VerifyAddedTests.cs
+++ src/DrMock.EfCore.Tests/VerifyTests/VerifyAddedTests.cs
@@ -75,74 +75,6 @@ namespace DrMock.EfCore.Tests.VerifyTests
         }
 
         [Fact]
-        public async Task GivenInterface_WhenAddedAsyncDirect_ShouldVerifyAddedCorrectly()
-        {
-            // Arrange
-            var mock = new MockDbContext<ITestDbContext>()
-                                .WithEntity<Person>();
-
-            var sut = mock.Object;
-
-            var firstName = RandomFirstName();
-            var lastName = RandomLastName();
-
-            Person newPerson = new Person() { FirstName = firstName, LastName = lastName };
-
-            // Act
-            await sut.AddAsync(newPerson);
-
-            // Assert
-            mock.VerifyAddedAsync<Person>(x => x.FirstName == firstName
-                                       && x.LastName == lastName);
-
-            mock.VerifyAddedAsync<Person>(x => x.FirstName == firstName
-                                       && x.LastName == lastName, Times.Once());
-
-            mock.VerifyAddedOnceAsync<Person>(x => x.FirstName == firstName
-                                       && x.LastName == lastName);
-
-            Should.Throw<Exception>(() => mock.VerifyNeverAdded<Person>(x => x.FirstName == firstName
-                                                                          && x.LastName == lastName));
-
-            mock.VerifyNeverAdded<Person>(x => x.FirstName == string.Empty
-                                            && x.LastName == string.Empty);
-        }
-
-        [Fact]
-        public async Task GivenClass_WhenAddedAsyncDirect_ShouldVerifyAddedCorrectly()
-        {
-            // Arrange
-            var mock = new MockDbContext<TestDbContext>()
-                                .WithEntity<Person>();
-
-            var sut = mock.Object;
-
-            var firstName = RandomFirstName();
-            var lastName = RandomLastName();
-
-            Person newPerson = new Person() { FirstName = firstName, LastName = lastName };
-
-            // Act
-            await sut.AddAsync(newPerson);
-
-            // Assert
-            mock.VerifyAddedAsync<Person>(x => x.FirstName == firstName
-                                       && x.LastName == lastName);
-
-            mock.VerifyAddedAsync<Person>(x => x.FirstName == firstName
-                                       && x.LastName == lastName, Times.Once());
-
-            mock.VerifyAddedOnceAsync<Person>(x => x.FirstName == firstName
-                                       && x.LastName == lastName);
-
-            Should.Throw<Exception>(() => mock.VerifyNeverAdded<Person>(x => x.FirstName == firstName
-                                                                          && x.LastName == lastName));
-
-            mock.VerifyNeverAdded<Person>(x => x.FirstName == string.Empty
-                                            && x.LastName == string.Empty);
-        }
-
-        [Fact]
         public void GivenInterface_WhenAddedOnDbSet_ShouldVerifyAddedCorrectly()
         {
             // Arrange
@@ -208,6 +140,126 @@ namespace DrMock.EfCore.Tests.VerifyTests
 
             mock.VerifyNeverAdded<Person>(x => x.FirstName == string.Empty
                                             && x.LastName == string.Empty);
+        }
+
+        [Fact]
+        public void GivenInterface_WhenAddedMultipleTimesDirect_ShouldVerifyAddedCorrectly()
+        {
+            // Arrange
+            var mock = new MockDbContext<ITestDbContext>()
+                                .WithEntity<Person>();
+
+            var sut = mock.Object;
+
+            var firstName = RandomFirstName();
+            var lastName = RandomLastName();
+
+            Person newPerson = new Person() { FirstName = firstName, LastName = lastName };
+
+            var numberOfEvents = RandomByteBetween(2, 5);
+
+            // Act
+            for (int i = 0; i < numberOfEvents; i++)
+            {
+                sut.Add(newPerson);
+            }
+
+            // Assert
+            mock.VerifyAdded<Person>(x => x.FirstName == firstName
+                                       && x.LastName == lastName, Times.Exactly(numberOfEvents));
+
+            Should.Throw<Exception>(() => mock.VerifyAddedOnce<Person>(x => x.FirstName == firstName
+                                                                         && x.LastName == lastName));
+        }
+
+        [Fact]
+        public void GivenClass_WhenAddedMultipleTimesDirect_ShouldVerifyAddedCorrectly()
+        {
+            // Arrange
+            var mock = new MockDbContext<TestDbContext>()
+                                .WithEntity<Person>();
+
+            var sut = mock.Object;
+
+            var firstName = RandomFirstName();
+            var lastName = RandomLastName();
+
+            Person newPerson = new Person() { FirstName = firstName, LastName = lastName };
+
+            var numberOfEvents = RandomByteBetween(2, 5);
+
+            // Act
+            for (int i = 0; i < numberOfEvents; i++)
+            {
+                sut.Add(newPerson);
+            }
+
+            // Assert
+            mock.VerifyAdded<Person>(x => x.FirstName == firstName
+                                       && x.LastName == lastName, Times.Exactly(numberOfEvents));
+
+            Should.Throw<Exception>(() => mock.VerifyAddedOnce<Person>(x => x.FirstName == firstName
+                                                                         && x.LastName == lastName));
+        }
+
+        [Fact]
+        public void GivenInterface_WhenAddedMultipleTimesOnDbSet_ShouldVerifyAddedCorrectly()
+        {
+            // Arrange
+            var mock = new MockDbContext<ITestDbContext>()
+                                .WithEntity<Person>();
+
+            var sut = mock.Object;
+
+            var firstName = RandomFirstName();
+            var lastName = RandomLastName();
+
+            Person newPerson = new Person() { FirstName = firstName, LastName = lastName };
+
+            var numberOfEvents = RandomByteBetween(2, 5);
+
+            // Act
+            for (int i = 0; i < numberOfEvents; i++)
+            {
+                sut.Add(newPerson);
+            }
+
+            // Assert
+            mock.VerifyAdded<Person>(x => x.FirstName == firstName
+                                       && x.LastName == lastName, Times.Exactly(numberOfEvents));
+
+            Should.Throw<Exception>(() => mock.VerifyAddedOnce<Person>(x => x.FirstName == firstName
+                                                                         && x.LastName == lastName));
+        }
+
+        [Fact]
+        public void GivenClass_WhenAddedMultipleTimesOnDbSet_ShouldVerifyAddedCorrectly()
+        {
+            // Arrange
+            var mock = new MockDbContext<TestDbContext>()
+                                .WithEntity<Person>();
+
+            var sut = mock.Object;
+
+            var firstName = RandomFirstName();
+            var lastName = RandomLastName();
+
+            Person newPerson = new Person() { FirstName = firstName, LastName = lastName };
+
+            var numberOfEvents = RandomByteBetween(2, 5);
+
+            // Act
+            for (int i = 0; i < numberOfEvents; i++)
+            {
+                sut.Add(newPerson);
+            }
+
+            // Assert
+            mock.VerifyAdded<Person>(x => x.FirstName == firstName
+                                       && x.LastName == lastName, Times.Exactly(numberOfEvents));
+
+            Should.Throw<Exception>(() => mock.VerifyAddedOnce<Person>(x => x.FirstName == firstName
+                                                                         && x.LastName == lastName));
         }
     }
 }
